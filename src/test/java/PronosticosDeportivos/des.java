5efbf7d4de932fa9f820a31beea6bbc35236e167
gespinach <gabrielaespinach@yahoo.com.ar>
2023-04-24 21:33:05 -0300

    1

---
+++ src/test/java/PronosticosDeportivos/des.java
@@ -0,0 +1,14 @@
+package PronosticosDeportivos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class des {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
